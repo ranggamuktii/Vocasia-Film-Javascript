d3ee9cd1b02f9062e27ee784414bb1c3f1fcb8ed
Rangga Mukti <r4n994mukti@gmail.com>
2024-10-05 21:35:47 +0700

    Memperbaiki Error Fetch JSON Keenam

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let dataFilm = [];
-fetch('/dataFilm.json')
+fetch('/dataFilm.JSON')
   .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
